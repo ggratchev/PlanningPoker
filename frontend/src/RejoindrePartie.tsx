7d4db52711b1253aeb3e1251367e89b5e7003fa5
Grigori <ggratchev.pro@gmail.com>
2025-12-01 17:48:14 +0100

    add pin code input in join game page

--- frontend/src/RejoindrePartie.tsx
+++ frontend/src/RejoindrePartie.tsx
@@ -2,20 +2,29 @@ import { useState } from 'react'
 
 function RejoindrePartie() {
     const [pseudo, setPseudo] = useState('')
+    const [code, setCode] = useState('')
 
     const valider = () => {
         console.log("pseudo :", pseudo)
+        console.log("code de la partie :", code)
     }
 
     return (
         <div>
             <h1>Rejoindre une partie</h1>
             <input type="text"
+                placeholder='Entrez le code de la partie'
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+            />
+            <input type="text"
                 placeholder='Entrez votre pseudo'
                 value={pseudo}
                 onChange={(e) => setPseudo(e.target.value)}
             />
-            <button onClick={valider} disabled={pseudo.trim() === ''}>Valider</button>
+            <button onClick={valider} disabled={
+                pseudo.trim() === '' ||
+                code.trim() === ''}>Valider</button>
         </div>
     )
 }
