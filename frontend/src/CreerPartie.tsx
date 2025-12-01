ee1cd7ae659be4a0229e99d11a97b69fbc1dcec1
Grigori <ggratchev.pro@gmail.com>
2025-12-01 12:59:13 +0100

    add name input

--- frontend/src/CreerPartie.tsx
+++ frontend/src/CreerPartie.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react'
+
 function CreerPartie() {
-  return <h1>Créer une partie</h1>
+    const [pseudo, setPseudo] = useState('')
+
+    const valider = () => {
+        console.log("pseudo :", pseudo)
+    }
+
+    return (
+        <div>
+            <h1>Créer une partie</h1>
+            <input type="text"
+                placeholder='Entrez votre pseudo'
+                value={pseudo}
+                onChange={(e) => setPseudo(e.target.value)}
+            />
+            <button onClick={valider}>Valider</button>
+        </div>
+    )
 }
 
 export default CreerPartie
