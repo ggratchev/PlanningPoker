74eaa90225223c616afa6b209b91c1bd12354300
Grigori <ggratchev.pro@gmail.com>
2025-12-01 17:23:48 +0100

    add random pin generation function in backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,39 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+import random
+
 app = Flask(__name__)
 CORS(app)
+
+parties = {}
+
+def generer_code():
+    while True:
+        code = str(random.randint(1000, 9999))
+        if code not in parties:
+            return code
 
 @app.route('/api/creer-partie', methods=['POST'])
 def creer_partie():
     data = request.json
     pseudo_createur = data.get('pseudo')
     mode_de_jeu = data.get('modeDeJeu')
-    
-    print(f"Pseudo du créateur: {pseudo_createur}")
-    print(f"Mode de jeu: {mode_de_jeu}")
-    
+    code_partie = generer_code()
+
+    #print(f"Pseudo du créateur: {pseudo_createur}")
+    #print(f"Mode de jeu: {mode_de_jeu}")
+    #print(code_partie)
+
+    parties[code_partie] = {
+        'createur': pseudo_createur,
+        'modeDeJeu': mode_de_jeu,
+        'participants': [pseudo_createur]
+    }
+
+    #print(parties[code_partie])
+
+    print("toutes les parties:", parties)
     return jsonify({
         'message': 'Partie créée'
     })
