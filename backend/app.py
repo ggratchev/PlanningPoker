4627f70947677b8b4eab0b48970d3eef67252d1d
Grigori Gratchev <grigori.gratchev64@gmail.com>
2025-12-18 22:10:46 +0100

    add gunicorn in requirements to host on render.rom

--- backend/app.py
+++ backend/app.py
@@ -249,4 +249,6 @@ def verification_votes(code):
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, port=port, host='0.0.0.0')
