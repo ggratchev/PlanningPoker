c52fe6068fe441802fa10d24bf68ec400e223a2b
Gaston <gagacoc89@gmail.com>
2025-12-18 22:57:07 +0100

    Preparing RejoindrePartie.tsx for future css.

--- frontend/src/RejoindrePartie.tsx
+++ frontend/src/RejoindrePartie.tsx
@@ -8,16 +8,30 @@ function RejoindrePartie() {
     }
 
     return (
-        <div>
-            <h1>Rejoindre une partie</h1>
-            <input type="text"
-                placeholder='Entrez votre pseudo'
-                value={pseudo}
-                onChange={(e) => setPseudo(e.target.value)}
-            />
-            <button onClick={valider} disabled={pseudo.trim() === ''}>Valider</button>
+    <div className="Rejoindre_partie">
+      <div className="carte_rejoindre_partie">
+        <h1 className="titre_rejoindre_partie">Rejoindre une partie</h1>
+
+        <div className="ligne_formulaire_rejoindre">
+          <input
+            className="champ_texte_rejoindre"
+            type="text"
+            placeholder="Entrez votre pseudo"
+            value={pseudo}
+            onChange={(e) => setPseudo(e.target.value)}
+          />
+
+          <button
+            className="bouton_valider_rejoindre"
+            onClick={valider}
+            disabled={pseudo.trim() === ""}
+          >
+            Valider
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default RejoindrePartie
